d25cf079756c7f5afc5369baadacedd8a52525da
Guarav Singh <gaurav.singh@razorpay.com>
2019-07-02 18:29:07 +0530

    Basic Setup

---
+++ database/migrations/2019_07_02_112538_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_id');
+            $table->string('name');
+            //$table->string('shared');
+            //$table->string('status');
+            $table->string('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h1 class="jsx-1202459474 title-lines-wrapper"><span style="color:#333" class="jsx-1202459474 title-line first">A new way
+                        </span><span style="color:#333" class="jsx-1202459474 title-line">to manage your tasks</span></h1>
+                </div>
+
+                <div class="panel-body">
+                    <span style="color:#333" class="jsx-1202459474 subtitle-line">Keep your work on track with</span>
+                    <span style="color:#333" class="jsx-1202459474 subtitle-line">a task management tool</span>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
